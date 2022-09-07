7b5bbe2325ed72f9bab011f43ab3a50b523ea9dc
maliuppal <ali.uppal@emumba.com>
2022-09-07 20:13:50 +0500

    structural changes

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { getStockLevel } from './services/stock-level';
+import { stockLevel } from './types/stockLevel.type';
+
+getStockLevel('SAL508741/19/43')
+    .then((res: stockLevel) => {
+        console.log(`Stock Level: ${JSON.stringify(res)}`);
+    })
+    .catch((err: any) => {
+        console.log(err);
+    });
