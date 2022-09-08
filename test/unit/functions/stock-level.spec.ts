d9e3dd134b94e9fa7ea68fabbfa80bd505ad96e8
maliuppal <ali.uppal@emumba.com>
2022-09-08 19:05:34 +0500

    more tests added

--- test/unit/functions/stock-level.spec.ts
+++ test/unit/functions/stock-level.spec.ts
@@ -1,4 +1,5 @@
-import { getStockLevel } from "../../../src/functions/stock-level";
+import { getStockLevel } from '../../../src/functions/stock-level';
+import { stockLevel } from '../../../src/types/stockLevel.type';
 
 const { expect } = require('chai');
 
@@ -6,7 +7,7 @@ describe('Testing the stock level', function () {
     it('it should return stock level object with sku and qty', async () => {
         const sku: string = 'SAL508741/19/43';
 
-        const stockLevel = await getStockLevel(sku);
+        const stockLevel: stockLevel = await getStockLevel(sku);
         expect(stockLevel).to.be.an('object');
     });
 
@@ -15,7 +16,20 @@ describe('Testing the stock level', function () {
         try {
             await getStockLevel(sku);
         } catch (error) {
-            expect(error).to.equal("Sku does not exists!")
-        }  
+            expect(error).to.equal('Sku does not exists!');
+        }
+    });
+
+    it('it should not return stock which should be greater then zero.', async () => {
+        const sku: string = 'SAL508741/19/43';
+        const stockLevel: stockLevel = await getStockLevel(sku);
+        expect(stockLevel.qty).to.be.greaterThan(0);
+    });
+
+    it('it should not return stock which should be greater then zero.', async () => {
+        const sku: string = 'SAL508741/19/43';
+        const stockLevel: stockLevel = await getStockLevel(sku);
+
+        expect(stockLevel.sku).to.be.equal(sku);
     });
 });
