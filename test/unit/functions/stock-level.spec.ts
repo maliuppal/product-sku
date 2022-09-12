163b6826c7a0168dd6a04fb66d03d176275a6aca
maliuppal <ali.uppal@emumba.com>
2022-09-12 16:25:51 +0500

    mocking tests

--- test/unit/functions/stock-level.spec.ts
+++ test/unit/functions/stock-level.spec.ts
@@ -1,13 +1,28 @@
 import { getStockLevel } from '../../../src/functions/stock-level';
 import { StockLevel } from '../../../src/types/StockLevel.type';
+import * as sinon from 'sinon'; 
+import * as stocksData from '../../../src/functions/stock.service';
+import * as transactionData from '../../../src/functions/transaction.service';
 
 const { expect } = require('chai');
 
 describe('Testing the stock level', function () {
+
     it('it should return stock level object with sku and qty', async () => {
         const sku: string = 'SAL508741/19/43';
 
+        // spy stocks
+        let spyStocks = sinon.spy(stocksData, "fetchStocksData");
+        spyStocks.calledWith(sku)
+
+        // spy transactions
+        let spyTransactions = sinon.spy(transactionData, "fetchTransactionsData");
+        spyStocks.calledWith(sku)
+
         const StockLevel: StockLevel = await getStockLevel(sku);
+
+        sinon.assert.calledOnce(spyStocks);
+        sinon.assert.calledOnce(spyTransactions);
         expect(StockLevel).to.be.an('object');
     });
 
